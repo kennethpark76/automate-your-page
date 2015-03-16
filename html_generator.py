fdf1bdd8bb4c26d8579a1c8e0a21d2f09809e49c
kennethpark76 <kennethpark@gmail.com>
2015-03-16 14:09:08 -0700

    Committed by Udacity

---
+++ html_generator.py
@@ -0,0 +1,33 @@
+LIST_OF_CONCEPTS = [ ['Python', 'Python is a Programming Language'],
+                    ['For Loop', 'For Loops allow you to iterate over lists'],
+                    ['Lists', 'Lists are sequences of data'] ]
+
+def generate_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def make_HTML(concept):
+    concept_title = concept[0]
+    concept_description = concept[1]
+    return generate_HTML(concept_title, concept_description)
+
+def print_HTML_for_all_concepts(list_of_concepts):
+    HTML = ""
+    for concept in list_of_concepts:
+        concept_HTML = make_HTML(concept)
+        HTML = HTML + concept_HTML
+    return HTML
+
+print print_HTML_for_all_concepts(LIST_OF_CONCEPTS)
